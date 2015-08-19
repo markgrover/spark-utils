1a8152e9303def012f5d52705b3f6ad40a312f42
Mark Grover <mgrover@cloudera.com>
2015-08-19 10:20:10 -0700

    Adding some comments

--- deploy_new_spark_tarball.sh
+++ deploy_new_spark_tarball.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -x
 DEFAULT_TARBALL_NAME=spark-1.5.0-SNAPSHOT-bin-nm.tgz
-# Must have password less SSH setup between all nodes of the cluster. In particular
+# * Must scp the tarball to one of the nodes of the cluster (can be any node)
+# * Must also scp this script to the same node.
+# * It's assumed that both this tarball and the script are present in the home directory
+# * The first optional parameter to this script is the name of the spark tarball
+# * The rest of the arguments to the script are the names of all of the nodes in the cluster
+# e.g. mynodes-{1..4}.mycompany.com 
+# * Must have password less SSH setup between all nodes of the cluster. In particular
 # it needs to be setup from the node where this script is going to run to all other
 # nodes of the cluster. This is only O(n) connections, other connections are not required
 DEFAULT_HOSTNAMES=($(echo mgrover-haa3-{1..4}.vpc.cloudera.com))
